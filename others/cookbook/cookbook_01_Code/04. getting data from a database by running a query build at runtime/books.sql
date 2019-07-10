f673981895c2c0708f738122a950643652e545c9
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2019-07-10 22:40:27 +0200

    add samples and gitignore file

---
+++ others/cookbook/cookbook_01_Code/04. getting data from a database by running a query build at runtime/books.sql
@@ -0,0 +1,23 @@
+﻿drop database books;
+create database books;
+use books;
+
+CREATE TABLE authors
+(
+  lastname TINYTEXT NOT NULL
+, firstname TINYTEXT NOT NULL
+, nationality VARCHAR(50) DEFAULT 'unknown'
+, birthyear INT default 1900
+, id_author CHAR(6) PRIMARY KEY
+)
+;
+
+CREATE TABLE books
+(
+  id_title CHAR(7) PRIMARY KEY
+, title TINYTEXT NOT NULL
+, id_author TINYTEXT NOT NULL
+, price DOUBLE DEFAULT 0
+, genre VARCHAR(30) DEFAULT 'unknown'
+)
+;
